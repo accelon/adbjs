9ea37a9c52be2704de30bb421b58650627596954
yapcheahshen <yapcheahshen@gmail.com>
2023-03-19 09:33:17 +0800

    handle adb has no source filename

--- src/dump.js
+++ src/dump.js
@@ -1,17 +1,23 @@
-const breakxml=s=>{
-    let prev=0, at=s.indexOf('<檔'), prevname='';
+const breakxml=(s,dbname)=>{
+    let prev=0, at=s.indexOf('<檔'), prevname=dbname+'.xml';
     const encoder=new TextEncoder();
     const out=[];
+    let count=0;
     while (at>=0) {
         let tagline=s.slice(at,at+200);
         const at2=tagline.indexOf('">');
         if (~at2) tagline=tagline.slice(0,at2+2);
-        const name=tagline.match(/n="(.+?)"/)[1];
-
+        let name=tagline.match(/n="(.+?)"/);
+        if (!name) {
+            name=dbname+'.'+count+'.xml';
+        } else {
+            name=name[1];
+        }
         if (at>prev) {
             const content=encoder.encode(s.slice(prev,at));
             out.push({name:prevname, content});
         }
+        count++;
         prev=at;
         prevname=name;
         at=s.indexOf('<檔',prev+3);
@@ -22,7 +28,7 @@ const breakxml=s=>{
 }
 
 export const dumpXML=adb=>{
-    const files=breakxml(adb.getXML());
+    const files=breakxml(adb.getXML(),adb.header.dbname);
     if (files.length>1) {
         const content=new TextEncoder().encode(files.map(it=>it.name).join('\n'));
         files.push({ name: adb.header.dbname+'.lst', content});
